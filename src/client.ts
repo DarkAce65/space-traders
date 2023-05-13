388bba36b53cd3c58c7f2683f045b153c033bb5f
DarkAce65 <tvdood99@gmail.com>
2023-05-13 16:31:06 -0400

    Add RTK as a dependency

--- src/client.ts
+++ src/client.ts
@@ -3,16 +3,15 @@ import createClient from 'openapi-fetch';
 
 import { paths } from '@/schema';
 
-const client = createClient<paths>({ baseUrl: 'https://api.spacetraders.io/v2' });
-
 const limiter = new Bottleneck({
   reservoir: 2,
   reservoirRefreshAmount: 2,
   reservoirRefreshInterval: 1000,
 });
 
-export const rateLimitedClient = new Proxy(client, {
-  get(_, key: keyof typeof client) {
-    return limiter.wrap(client[key]);
+const baseClient = createClient<paths>({ baseUrl: 'https://api.spacetraders.io/v2' });
+export const client = new Proxy(baseClient, {
+  get(_, key: keyof typeof baseClient) {
+    return limiter.wrap(baseClient[key]);
   },
 });
