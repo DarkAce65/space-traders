289780de67e920aa7e2c119380962a5b643cefcf
DarkAce65 <tvdood99@gmail.com>
2023-05-13 16:06:16 -0400

    Set up rate limited client

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,18 @@
+import Bottleneck from 'bottleneck';
 import createClient from 'openapi-fetch';
 
 import { paths } from '@/schema';
 
-export const client = createClient<paths>({ baseUrl: 'https://api.spacetraders.io/v2' });
+const client = createClient<paths>({ baseUrl: 'https://api.spacetraders.io/v2' });
+
+const limiter = new Bottleneck({
+  reservoir: 2,
+  reservoirRefreshAmount: 2,
+  reservoirRefreshInterval: 1000,
+});
+
+export const rateLimitedClient = new Proxy(client, {
+  get(_, key: keyof typeof client) {
+    return limiter.wrap(client[key]);
+  },
+});
