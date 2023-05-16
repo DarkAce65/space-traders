2a530124fc70501c6b167a6bfb225849de3f55e9
DarkAce65 <tvdood99@gmail.com>
2023-05-16 06:30:48 -0400

    Set up agent fetch thunk

--- src/data/agentSlice.ts
+++ src/data/agentSlice.ts
@@ -1,14 +1,44 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-export type AgentState = {
+import { getAuthToken } from './authSlice';
+import { registerAgent } from './globalActions';
+import { createAppAsyncThunk } from './storeUtils';
+import { client, unwrapDataOrThrow } from '../client';
+
+export interface AgentState {
   accountId: string;
   agentName: string;
   headquarters: string;
   credits: number;
-} | null;
+}
 
-const initialState = null as AgentState;
+export const fetchAgent = createAppAsyncThunk('agent/fetch', (_, { getState }) => {
+  const token = getAuthToken(getState());
+  return client
+    .get('/my/agent', { headers: { Authorization: `Bearer ${token}` } })
+    .then(unwrapDataOrThrow);
+});
 
-const agentSlice = createSlice({ name: 'agent', initialState, reducers: {} });
+const initialState: AgentState = {
+  accountId: '',
+  agentName: '',
+  headquarters: '',
+  credits: 0,
+};
+
+const agentSlice = createSlice({
+  name: 'agent',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(registerAgent.fulfilled, (state, action) => {
+      const { accountId, symbol, headquarters, credits } = action.payload.data.agent;
+      state.accountId = accountId;
+      state.agentName = symbol;
+      state.headquarters = headquarters;
+      state.credits = credits;
+    });
+  },
+});
 
 export default agentSlice.reducer;
