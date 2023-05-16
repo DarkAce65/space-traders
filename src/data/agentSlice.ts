8b560e154075525248961593d77f1d17fd774ec4
DarkAce65 <tvdood99@gmail.com>
2023-05-16 06:10:51 -0400

    Set up agent slice

---
+++ src/data/agentSlice.ts
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+type AgentState = {
+  accountId: string;
+  agentName: string;
+  headquarters: string;
+  credits: number;
+} | null;
+
+const initialState = null as AgentState;
+
+const agentSlice = createSlice({ name: 'agent', initialState, reducers: {} });
+
+export default agentSlice.reducer;
--- src/data/store.ts
+++ src/data/store.ts
@@ -1,8 +1,9 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
+import agent from './agentSlice';
 import auth from './authSlice';
 
-const reducer = combineReducers({ auth });
+const reducer = combineReducers({ agent, auth });
 export const createStore = () => configureStore({ devTools: true, reducer });
 
 export type RootState = ReturnType<typeof reducer>;
