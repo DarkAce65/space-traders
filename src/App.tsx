a3f51cc2952fa6669cfbe027c7ab520a1b9824ad
DarkAce65 <tvdood99@gmail.com>
2023-05-18 16:25:24 -0400

    Save store to file level variable to prevent duplicate stores

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
-import { Store } from '@reduxjs/toolkit';
-import { Component, ReactNode } from 'react';
 import { Provider } from 'react-redux';
 
-import { RootState, createStore } from './data/store';
+import { createStore } from './data/store';
 
-class App extends Component {
-  private store: Store<RootState>;
+const store = createStore();
 
-  constructor(props: never) {
-    super(props);
-    this.store = createStore();
-  }
-
-  render(): ReactNode {
-    return <Provider store={this.store}></Provider>;
-  }
-}
+const App = () => <Provider store={store}></Provider>;
 
 export default App;
