6e989b16ad6100ec18baf64f7d84a757ab704ce1
DarkAce65 <tvdood99@gmail.com>
2023-05-18 17:03:46 -0400

    Add ship data to store

---
+++ src/data/slices/shipsSlice.ts
@@ -0,0 +1,48 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import { external } from '@/schema';
+
+import { client, unwrapDataOrThrow } from '../client';
+import { getAuthToken } from '../selectors';
+
+import { createAppAsyncThunk } from '../storeUtils';
+import { pagedFetchAll } from '../pagedFetchAll';
+
+type Ship = external['../models/Ship.json'];
+
+export interface ShipsState {
+  ships: { [shipSymbol: string]: Ship };
+}
+
+export const fetchAllShips = createAppAsyncThunk('ships/fetchAllShips', async (_, { getState }) => {
+  const token = getAuthToken(getState());
+  return pagedFetchAll(
+    (page, limit) =>
+      client
+        .get('/my/ships', {
+          headers: { Authorization: `Bearer ${token}` },
+          params: { query: { page, limit } },
+        })
+        .then(unwrapDataOrThrow)
+        .then((response) => ({ data: response.data, total: response.meta.total })),
+    20
+  );
+});
+
+const initialState: ShipsState = { ships: {} };
+
+const shipsSlice = createSlice({
+  name: 'ships',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchAllShips.fulfilled, (state, action) => {
+      const ships = action.payload.data;
+      for (const ship of ships) {
+        state.ships[ship.symbol] = ship;
+      }
+    });
+  },
+});
+
+export default shipsSlice.reducer;
--- src/data/store.ts
+++ src/data/store.ts
@@ -2,9 +2,11 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 import agent from './slices/agentSlice';
 import auth from './slices/authSlice';
+
+import ships from './slices/shipsSlice';
 import universe from './slices/universeSlice';
 
-const reducer = combineReducers({ agent, auth, universe });
+const reducer = combineReducers({ agent, auth, ships, universe });
 export const createStore = () => configureStore({ devTools: true, reducer });
 
 export type RootState = ReturnType<typeof reducer>;
