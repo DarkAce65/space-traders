05a734322acd55069cbba8bcf8a44f744186bbe0
DarkAce65 <tvdood99@gmail.com>
2023-05-18 16:17:33 -0400

    Add actions to scan for objects

--- src/data/actions.ts
+++ src/data/actions.ts
@@ -9,7 +9,7 @@ type RegisterAgentBody = NonNullable<
 type RegisterAgentArgs = {
   agentName: RegisterAgentBody['symbol'];
   faction: RegisterAgentBody['faction'];
-  email: RegisterAgentBody['email'];
+  email?: RegisterAgentBody['email'];
 };
 export const registerAgent = createAppAsyncThunk(
   'auth/register',
@@ -17,4 +17,22 @@ export const registerAgent = createAppAsyncThunk(
     client
       .post('/register', { body: { symbol: agentName, faction, email } })
       .then(unwrapDataOrThrow)
+);
+
+export const scanSystems = createAppAsyncThunk('ships/scanSystems', (shipSymbol: string) =>
+  client
+    .post('/my/ships/{shipSymbol}/scan/systems', { params: { path: { shipSymbol } } })
+    .then(unwrapDataOrThrow)
+);
+
+export const scanWaypoints = createAppAsyncThunk('ships/scanWaypoints', (shipSymbol: string) =>
+  client
+    .post('/my/ships/{shipSymbol}/scan/waypoints', { params: { path: { shipSymbol } } })
+    .then(unwrapDataOrThrow)
+);
+
+export const scanShips = createAppAsyncThunk('ships/scanShips', (shipSymbol: string) =>
+  client
+    .post('/my/ships/{shipSymbol}/scan/ships', { params: { path: { shipSymbol } } })
+    .then(unwrapDataOrThrow)
 );
