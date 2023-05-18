dca27c65321b1484e867c953949be98c9e5f1c97
DarkAce65 <tvdood99@gmail.com>
2023-05-18 16:20:05 -0400

    Restructure universe data and add reducers for fetches and scans

--- src/data/slices/universeSlice.ts
+++ src/data/slices/universeSlice.ts
@@ -3,20 +3,33 @@ import { createSlice } from '@reduxjs/toolkit';
 import { external } from '@/schema';
 
 import { client, unwrapDataOrThrow } from '../../client';
+import { scanSystems, scanWaypoints } from '../actions';
+import { pagedFetchAll } from '../pagedFetchAll';
 import { getAuthToken } from '../selectors';
 import { RootState } from '../store';
 import { createAppAsyncThunk } from '../storeUtils';
 
+type UnexploredSystem = external['../models/ScannedSystem.json'];
+type UnexploredWaypoint = external['../models/SystemWaypoint.json'];
 type System = external['../models/System.json'];
-type SystemWaypoint = external['../models/SystemWaypoint.json'];
 type Waypoint = external['../models/Waypoint.json'];
 
 export interface UniverseState {
-  systems: { [systemSymbol: string]: System };
-  waypoints: { [waypointSymbol: string]: SystemWaypoint | Waypoint };
+  systems: {
+    [systemSymbol: string]:
+      | { isExplored: true; data: System }
+      | { isExplored: false; data: UnexploredSystem };
+  };
+  waypoints: {
+    [waypointSymbol: string]:
+      | { isExplored: true; data: Waypoint }
+      | { isExplored: false; data: UnexploredWaypoint };
+  };
 }
 
 const getSystems = (state: RootState) => state.universe.systems;
+
+const getWaypoints = (state: RootState) => state.universe.waypoints;
 
 export const fetchSystem = createAppAsyncThunk(
   'universe/fetchSystem',
@@ -34,6 +47,52 @@ export const fetchSystem = createAppAsyncThunk(
   }
 );
 
+export const fetchWaypoint = createAppAsyncThunk(
+  'universe/fetchWaypoint',
+  async (
+    { systemSymbol, waypointSymbol }: { systemSymbol: string; waypointSymbol: string },
+    { getState }
+  ) => {
+    const token = getAuthToken(getState());
+    const response = await client.get('/systems/{systemSymbol}/waypoints/{waypointSymbol}', {
+      headers: { Authorization: `Bearer ${token}` },
+      params: { path: { systemSymbol, waypointSymbol } },
+    });
+    return unwrapDataOrThrow(response);
+  },
+  {
+    condition: ({ waypointSymbol }, { getState }) =>
+      !Object.prototype.hasOwnProperty.call(getWaypoints(getState()), waypointSymbol),
+  }
+);
+
+export const fetchSystemWaypoints = createAppAsyncThunk(
+  'universe/fetchSystemWaypoints',
+  (systemSymbol: string, { getState }) => {
+    const token = getAuthToken(getState());
+    return pagedFetchAll(
+      (page, limit) =>
+        client
+          .get('/systems/{systemSymbol}/waypoints', {
+            headers: { Authorization: `Bearer ${token}` },
+            params: { path: { systemSymbol }, query: { limit, page } },
+          })
+          .then(unwrapDataOrThrow)
+          .then((response) => ({ data: response.data, total: response.meta.total })),
+      20
+    );
+  },
+  {
+    condition: (systemSymbol, { getState }) => {
+      const systemsBySymbol = getSystems(getState());
+      return (
+        !Object.prototype.hasOwnProperty.call(systemsBySymbol, systemSymbol) ||
+        !systemsBySymbol[systemSymbol].isExplored
+      );
+    },
+  }
+);
+
 const initialState: UniverseState = { systems: {}, waypoints: {} };
 
 const universeSlice = createSlice({
@@ -41,14 +100,56 @@ const universeSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(fetchSystem.fulfilled, (state, action) => {
-      const system = action.payload.data;
-      state.systems[system.symbol] = system;
+    builder
+      .addCase(fetchSystem.fulfilled, (state, action) => {
+        const system = action.payload.data;
+        state.systems[system.symbol] = { isExplored: true, data: system };
 
-      for (const waypoint of system.waypoints) {
-        state.waypoints[waypoint.symbol] = waypoint;
-      }
-    });
+        for (const waypoint of system.waypoints) {
+          if (!Object.prototype.hasOwnProperty.call(state.waypoints, waypoint.symbol)) {
+            state.waypoints[waypoint.symbol] = { isExplored: false, data: waypoint };
+          }
+        }
+      })
+      .addCase(fetchWaypoint.fulfilled, (state, action) => {
+        const waypoint = action.payload.data;
+        if (
+          !Object.prototype.hasOwnProperty.call(state.waypoints, waypoint.symbol) ||
+          !state.waypoints[waypoint.symbol].isExplored
+        ) {
+          state.waypoints[waypoint.symbol] = { isExplored: true, data: waypoint };
+        }
+      })
+      .addCase(fetchSystemWaypoints.fulfilled, (state, action) => {
+        const waypoints = action.payload.data;
+        for (const waypoint of waypoints) {
+          if (
+            !Object.prototype.hasOwnProperty.call(state.waypoints, waypoint.symbol) ||
+            !state.waypoints[waypoint.symbol].isExplored
+          ) {
+            state.waypoints[waypoint.symbol] = { isExplored: true, data: waypoint };
+          }
+        }
+      })
+      .addCase(scanSystems.fulfilled, (state, action) => {
+        const scannedSystems = action.payload.data.systems;
+        for (const system of scannedSystems) {
+          if (!Object.prototype.hasOwnProperty.call(state.systems, system.symbol)) {
+            state.systems[system.symbol] = { isExplored: false, data: system };
+          }
+        }
+      })
+      .addCase(scanWaypoints.fulfilled, (state, action) => {
+        const scannedWaypoints = action.payload.data.waypoints;
+        for (const waypoint of scannedWaypoints) {
+          if (
+            !Object.prototype.hasOwnProperty.call(state.waypoints, waypoint.symbol) ||
+            !state.waypoints[waypoint.symbol].isExplored
+          ) {
+            state.waypoints[waypoint.symbol] = { isExplored: true, data: waypoint };
+          }
+        }
+      });
   },
 });
 
