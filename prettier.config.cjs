04f4b564487dce7f87aaec40f9a45dfd569df30b
DarkAce65 <tvdood99@gmail.com>
2023-05-24 01:06:44 +0530

    Set up tailwind with daisyui

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,5 @@
 module.exports = {
   singleQuote: true,
   printWidth: 100,
+  plugins: [require('prettier-plugin-tailwindcss')],
 };
---
+++ tailwind.config.cjs
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  plugins: [require('daisyui')],
+  daisyui: { themes: ['night'] },
+};
