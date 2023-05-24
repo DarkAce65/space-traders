cd03c0948f9d73fba16dea50c416d4b150d65bad
DarkAce65 <tvdood99@gmail.com>
2023-05-24 22:55:40 +0530

    Add fetch status to agent store

--- src/data/slices/agentSlice.ts
+++ src/data/slices/agentSlice.ts
@@ -2,30 +2,48 @@ import { createSlice, isAnyOf } from '@reduxjs/toolkit';
 
 import { external } from '@/schema';
 
+import { LoadStatus } from '../../types';
 import assertUnreachable from '../../utils/assertUnreachable';
 import { registerAgent } from '../actions';
 import { client, unwrapDataOrThrow } from '../client';
 import { getAuthHeaderOrThrow } from '../selectors';
+import { RootState } from '../store';
 import { createAppAsyncThunk } from '../storeUtils';
 
 export interface AgentState {
-  accountId: string;
-  agentName: string;
-  headquarters: string;
-  credits: number;
+  registerAgentStatus: LoadStatus;
+  status: LoadStatus;
+  data: {
+    accountId: string;
+    agentName: string;
+    headquarters: string;
+    credits: number;
+  } | null;
 }
 
-export const fetchAgent = createAppAsyncThunk('agent/fetch', async (_, { getState }) => {
-  const headers = getAuthHeaderOrThrow(getState());
-  const response = await client.get('/my/agent', { headers });
-  return unwrapDataOrThrow(response);
-});
+export const getAgentFetchStatus = (state: RootState) => state.agent.status;
+
+export const getAgent = (state: RootState) => state.agent.data;
+
+export const fetchAgent = createAppAsyncThunk(
+  'agent/fetch',
+  async (_, { getState }) => {
+    const headers = getAuthHeaderOrThrow(getState());
+    const response = await client.get('/my/agent', { headers });
+    return unwrapDataOrThrow(response);
+  },
+  {
+    condition: (_, { getState }) => {
+      const fetchStatus = getAgentFetchStatus(getState());
+      return fetchStatus === 'UNINITIALIZED' || fetchStatus === 'FAILED';
+    },
+  }
+);
 
 const initialState: AgentState = {
-  accountId: '',
-  agentName: '',
-  headquarters: '',
-  credits: 0,
+  registerAgentStatus: 'UNINITIALIZED',
+  status: 'UNINITIALIZED',
+  data: null,
 };
 
 const agentSlice = createSlice({
@@ -33,22 +51,39 @@ const agentSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addMatcher(isAnyOf(registerAgent.fulfilled, fetchAgent.fulfilled), (state, action) => {
-      let agent: external['../models/Agent.json'];
-      if (registerAgent.fulfilled.match(action)) {
-        agent = action.payload.data.agent;
-      } else if (fetchAgent.fulfilled.match(action)) {
-        agent = action.payload.data;
-      } else {
-        throw assertUnreachable(action);
-      }
+    builder
 
-      const { accountId, symbol, headquarters, credits } = agent;
-      state.accountId = accountId;
-      state.agentName = symbol;
-      state.headquarters = headquarters;
-      state.credits = credits;
-    });
+      .addMatcher(isAnyOf(registerAgent.pending, fetchAgent.pending), (state, action) => {
+        if (registerAgent.fulfilled.match(action)) {
+          state.registerAgentStatus = 'PENDING';
+        }
+        state.status = 'PENDING';
+      })
+      .addMatcher(isAnyOf(registerAgent.fulfilled, fetchAgent.fulfilled), (state, action) => {
+        let agent: external['../models/Agent.json'];
+        if (registerAgent.fulfilled.match(action)) {
+          agent = action.payload.data.agent;
+          state.registerAgentStatus = 'SUCCEEDED';
+        } else if (fetchAgent.fulfilled.match(action)) {
+          agent = action.payload.data;
+        } else {
+          throw assertUnreachable(action);
+        }
+
+        state.status = 'SUCCEEDED';
+        state.data = {
+          accountId: agent.accountId,
+          agentName: agent.symbol,
+          headquarters: agent.headquarters,
+          credits: agent.credits,
+        };
+      })
+      .addMatcher(isAnyOf(registerAgent.rejected, fetchAgent.rejected), (state, action) => {
+        if (registerAgent.fulfilled.match(action)) {
+          state.registerAgentStatus = 'FAILED';
+        }
+        state.status = 'FAILED';
+      });
   },
 });
 
